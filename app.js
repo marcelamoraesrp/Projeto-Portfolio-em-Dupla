94ef24b5b0233cebb5c03109af422ac932843c84
João Luiz <ojoao.luiz07@gmail.com>
2026-03-25 16:11:58 -0300

    testando card pros projetos ( é só dar ctrl + ; em tudo lá na parte de projetos do html se quiser ver)

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const botao = document.getElementById('botao');
+const joaoclass = document.getElementById('joaoclass');
+const marcelaclass = document.getElementById('marcelaclass');
+
+let joaocard = 2;
+
+function quem() {
+    if (joaoclass.classList.contains('Inativo')) {
+        botao.textContent = "Clique para ver os projetos do João";
+    }
+    else {
+        botao.textContent = "Clique para ver os projetos da Marcela";
+    }
+}
+
+quem()
 
 function vai() {
-    const joao = document.getElementById('joao');
-    joao.classList.toggle('Inativo');
-    const marcela = document.getElementById('marcela');
-    marcela.classList.toggle('Inativo');
+    joaoclass.classList.toggle('Inativo');
+    marcelaclass.classList.toggle('Inativo');
+    quem()
 }
 
 botao.addEventListener('click', vai);
