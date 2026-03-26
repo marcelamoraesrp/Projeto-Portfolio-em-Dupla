cb918008ca65f4cfbf0493a914eb00ad492b21d1
marcelamoraesrp <marcela.moraesrp@gmail.com>
2026-03-26 20:41:58 -0300

    Menu toggle

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const botao = document.getElementById('botao');
+const btnMobile = document.getElementById('btn-mobile');
 const joaoclass = document.getElementById('joaoclass');
 const marcelaclass = document.getElementById('marcelaclass');
 
 let joaocard = 2;
+
+function toggleMenu(){
+    const nav = document.getElementById('barra-nav');
+    nav.classList.toggle('active');
+}
 
 function quem() {
     if (joaoclass.classList.contains('Inativo')) {
@@ -21,4 +27,5 @@ function vai() {
     quem()
 }
 
+btnMobile.addEventListener('click', toggleMenu)
 botao.addEventListener('click', vai);
